da3d0b468f213f7063b1a99afb4438e8cfb63544
Gabriel Gasperi Casagrande <gabriel.casagrande1@ufla.br>
2020-10-13 18:25:21 -0300

    :construction: alterado a estrutura de pastas; inicado backend

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
 
 import mapMarkerImg from '../images/mark.svg';
 
 import '../styles/pages/orphanages-map.css';
+import 'leaflet/dist/leaflet.css';
 
 function OrphanagesMap() {
   return (
@@ -23,7 +25,20 @@ function OrphanagesMap() {
         </footer>
       </aside>
 
-      <div></div>
+      <Map
+        center={[-22.5952714,-46.5360285]}
+        zoom={15}
+        style={
+          {
+            width: '100%',height: '100%'
+          }
+        }
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer 
+          url={`https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      </Map>
 
       <Link to="" className="create-orphanage" >
         <FiPlus size={32} color="#fff" />
