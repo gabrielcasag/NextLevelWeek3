012d3a81cebae60e0a6a66b820315e209b760fbc
Gabriel Casagrande <gaspericasag@gmail.com>
2020-10-13 11:59:13 -0300

    :rocket: iniciando roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,14 @@
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi';
 
 import './styles/global.css';
-import './styles/pages/landing.css';
-
-import logoImg from './images/logo.svg';
+import Routes from './routes';
 
 //componente react
 //uma funçao que retorna html ou jsx(tsx)
 
 function App() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        {/* para inserir uma variavel js dentro do html usa-se {} */}
-        <img src={logoImg} alt="Happy"/>
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>  
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>    
-
-        <div className="location">
-          <strong>Socorro</strong>
-          <span>São Paulo</span>
-        </div>
-
-        <a href="/" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-        </a>
-      </div>
-  </div>
+    <Routes />
   );
 }
 
