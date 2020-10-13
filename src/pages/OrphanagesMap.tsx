313c9cb1ec49895ef8fb257992ea918a6859ee40
Gabriel Casagrande <gaspericasag@gmail.com>
2020-10-13 15:50:59 -0300

    :rocket: iniciando a tela orphanage map

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import mapMarkerImg from '../images/mark.svg';
+
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
   return (
-    <h1>Hello orphanages</h1>
-  );
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy"/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Socorro</strong>
+          <span>São Paulo</span>
+        </footer>
+      </aside>
+
+      <div></div>
+
+      <Link to="" className="create-orphanage" >
+        <FiPlus size={32} color="#fff" />
+      </Link>      
+    </div>
+
+    );
 }
 
 export default OrphanagesMap;
