87f560b49a5218435131f69c148e69ccc03765dc
Gabriel Casagrande <gabriel.casagrande@estudante.ufla.br>
2020-10-13 23:58:37 -0300

    :construction: iniciando o database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
 
 app.use(express.json());
@@ -30,3 +32,9 @@ app.listen(3333);
 // QUERY PARAMS => enviados na rota, usados mais em filtros, paginacao ex: rota/users?page=2 
 // ROUTE PARAMS => enviados na rota tmb mas para identificar um recurso, ex: rota/users/1
 // BODY => no corpo da requisição, enviar dados de formulários por exemplo
+
+// 3 formas de lidar com banco de dados
+// Driver Nativo => permite executar as querys direto pelo node mas sem abstrações(sql puro) 
+// Query Builder => knex.js(+ famoso do node), escreve as querys com javascript, tipo o LINQ do C#(sintaxe mais amigavel)
+// ORM => maior abstrção, uma classe simbolizando a tabela no banco. 
+// Query e ORM => permitem trocar de SGBDs sem mudar a aplicação.
