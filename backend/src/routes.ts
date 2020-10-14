82b3c623ad71bf98e776220f146c2f19fd960b52
Gabriel Casagrande <gabriel.casagrande@estudante.ufla.br>
2020-10-14 12:07:58 -0300

    :rocket: adicionando camada controller

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,37 +1,13 @@
 import { Router } from 'express';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
+import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
 
 // criando uma ROTA com RECURSOS de usuario
-routes.post('/orphanages', async (req, res) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends
-  } = req.body;
+routes.post('/orphanages', OrphanagesController.create);
+routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
 
-  const orphanagesRepository = getRepository(Orphanage);
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends
-  });
-
-  await orphanagesRepository.save(orphanage);
-
-  return res.status(201).json({ message: 'Orfanato Salvo com Sucesso!' });
-});
 
 export default routes;
 
@@ -39,3 +15,5 @@ export default routes;
 // M => models, manipulação dos dados, representaçao das entidades
 // V => views, como as coisas sao visualizadas, frontend
 // C => controllers, logica das rotas
+// padrao dos controllers é ter os metodos para:
+// index, show, create, update, delete
